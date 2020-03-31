cc9d2e2cb1a0667d56b733b5541fb513ab3b1c5a
Pierre Vert <pierr65@gmail.com>
2020-03-31 18:12:06 +0200

    image & tax_layout only on category

--- inc/custom-acf-taxo-fields.php
+++ inc/custom-acf-taxo-fields.php
@@ -54,7 +54,7 @@ if( function_exists('acf_add_local_field_group') ):
                 array(
                     'param' => 'taxonomy',
                     'operator' => '==',
-                    'value' => 'all',
+                    'value' => 'category',
                 ),
             ),
         ),
