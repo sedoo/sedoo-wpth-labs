0ad7cbd7e6c1f8c1afd1bbf671df481c7bea9cff
Pierre Vert <pierr65@gmail.com>
2020-05-07 15:57:55 +0200

    info author fix

--- template-parts/single-author.php
+++ template-parts/single-author.php
@@ -45,14 +45,14 @@
             <?php 
             if ($select_lauteur_array['user_description']) {
             ?>
-            <h3><?php echo __("À propos de l'intervenant", 'sedoo-wpth-labs'); ?></h3>
+            <h3><?php echo __("À propos", 'sedoo-wpth-labs'); ?></h3>
             <p class="author-description">
                 <?php echo $select_lauteur_array['user_description'];?>
             </p> 
             <?php
             }
             ?>
-           <a href="<?php echo get_author_posts_url($select_lauteur_array['ID'], $select_lauteur_array['user_nicename'] ); ?>" class="btn"><?php echo __("En savoir plus sur l'intervenant", 'sedoo-wpth-labs'); ?></a>
+           <a href="<?php echo get_author_posts_url($select_lauteur_array['ID'], $select_lauteur_array['user_nicename'] ); ?>" class="btn"><?php echo __("En savoir plus", 'sedoo-wpth-labs'); ?></a>
         </div>
    </div>
 </div>
@@ -95,13 +95,13 @@
             <p><b><?php the_field('nom_de_lauteur_exterieur'); ?></b></p>
             <p><?php the_field('poste_de_lauteur'); ?></p>
             <?php if( get_field('a_propos_auteur') ){ ?>
-            <h3><?php echo __("À propos de l'intervenant", 'sedoo-wpth-labs'); ?></h3>
+            <h3><?php echo __("À propos", 'sedoo-wpth-labs'); ?></h3>
             <p class="author-description">
                 <?php the_field('a_propos_auteur'); ?>
             </p> 
             <?php } ?>
             <?php if( get_field('site_internet_auteur') ){ ?>
-                <a href="<?php the_field('site_internet_auteur'); ?>" class="btn" target="_blank"><?php echo __("En savoir plus sur l'intervenant", 'sedoo-wpth-labs'); ?></a>
+                <a href="<?php the_field('site_internet_auteur'); ?>" class="btn" target="_blank"><?php echo __("En savoir plus", 'sedoo-wpth-labs'); ?></a>
             <?php } ?>
         </div>
    </div>
