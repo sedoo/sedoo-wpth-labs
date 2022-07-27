c79d055073d822b1485f4837ecd20e896de6ef3c
Pierre Vert <pierr65@gmail.com>
2022-07-27 12:04:25 +0200

    bug fixed on display location shortcut

--- template-parts/shortcut.php
+++ template-parts/shortcut.php
@@ -6,7 +6,7 @@
 $options_theme = get_field('ajout_options', 'option');
 ?>
 <ul id="shortcuts">
-    <?php if( get_field('display_shortcut', 'option') == 'oui' ) { ?>
+    <?php if( get_field('display_map_shortcut', 'option') == 'oui' ) { ?>
     <li class="location-btn">
         <a href="<?php the_field('url_page_access', 'option'); ?>">
             <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 30 30" enable-background="new 0 0 30 30" xml:space="preserve">
